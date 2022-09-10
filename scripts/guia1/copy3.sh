66f98cd7415976224a37d32378fc3ddfaacf1ab2
Nicolas <silvanicolas93@gmail.com>
2022-09-09 21:26:39 -0300

    Agrego condiocionales de existencia para evitar errores en copy3.sh
    Signed-off-by: Nicolas <silvanicolas93@gmail.com>

--- scripts/guia1/copy3.sh
+++ scripts/guia1/copy3.sh
@@ -1,5 +1,13 @@
+if [ -e a/a.txt ] && [ -e a/b/b.txt ] && [ -e a/b/c/d.doc ] && [ -e a/b/c.mp3 ]
+then
+
 mkdir -p files
 cp a/a.txt files
 cp a/b/b.txt files
 cp a/b/c/d.doc files
 cp a/b/c.mp3 files
+
+else
+echo "falta al menos un archivo para copiar"
+
+fi
